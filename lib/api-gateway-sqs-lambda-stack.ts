b961bb2c48730fc1def1a14411222972b41e92a2
Kevin Shaffer-Morrison <shafkevi@amazon.com>
2022-02-16 12:25:16 -0500

    Adding in lib for apigw + sqs + lambda and lambda function code

--- lib/api-gateway-sqs-lambda-stack.ts
+++ lib/api-gateway-sqs-lambda-stack.ts
@@ -1,16 +1,68 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Duration, Aws } from 'aws-cdk-lib';
+import { aws_sqs as sqs } from 'aws-cdk-lib';
+import { aws_iam as iam } from 'aws-cdk-lib';
+import { aws_apigateway as apigateway } from 'aws-cdk-lib';
+import { aws_lambda as lambda } from 'aws-cdk-lib';
+import { aws_lambda_event_sources as lambda_event_sources } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ApiGatewaySqsLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const queue = new sqs.Queue(this, 'ApiGatewaySqsLambdaQueue', {
+      visibilityTimeout: Duration.seconds(300)
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApiGatewaySqsLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const restApiRole = new iam.Role(this, "RestApiRole", {
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSQSFullAccess")]
+    });
+
+    const baseApi = new apigateway.RestApi(this, "ApiGateway", {
+      restApiName: "SqsApi"
+    });
+
+    baseApi.root.addMethod("ANY")
+
+    const apiResource = baseApi.root.addResource('example')
+    const integrationResponse: apigateway.IntegrationResponse = {
+      statusCode:"200",
+      responseTemplates:{"application/json": ""},
+    }
+
+    const apiResourceSqsIntegration = new apigateway.AwsIntegration({
+      service: 'sqs',
+      path: `${Aws.ACCOUNT_ID}/${queue.queueName}`,
+      options: {
+        credentialsRole: restApiRole,
+        integrationResponses: [integrationResponse],
+        requestTemplates: {"application/json": "Action=SendMessage&MessageBody=$input.body"},
+        passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+        requestParameters:{"integration.request.header.Content-Type": "'application/x-www-form-urlencoded'"},
+      }
+    })
+
+    const methodResponse: apigateway.MethodResponse = {
+      statusCode: '200'
+    }
+
+    apiResource.addMethod(
+      "POST",
+      apiResourceSqsIntegration,
+      {methodResponses: [methodResponse]}
+    )
+
+    const sqsLambda = new lambda.Function(this, "SqsTriggerLambda", {
+      handler: "lambda-handler.handler",
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code:lambda.Code.fromAsset('lambda'),
+    })
+
+    const sqsEventSource = new lambda_event_sources.SqsEventSource(queue);
+
+    sqsLambda.addEventSource(sqsEventSource);
+
+
   }
 }
